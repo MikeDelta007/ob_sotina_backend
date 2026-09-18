aef924f450ad46d82a162115f51f9e09890732ab
fadeldiouf <diouffadel406@gmail.com>
2026-09-18 12:00:04 +0000

    sso

--- src/main/java/com/officedubac/project/controllers/PdfController.java
+++ src/main/java/com/officedubac/project/controllers/PdfController.java
@@ -82,24 +82,24 @@ public class PdfController
     @Autowired
     private PdfStatService pdfStatService;
 
-    @Autowired
-    private GabaritA1 gabaritA1;
+//    @Autowired
+//    private GabaritA1 gabaritA1;
+//
+//    @Autowired
+//    private GabaritOptionT2 gabaritOptionT2;
 
-    @Autowired
-    private GabaritOptionT2 gabaritOptionT2;
 
-
-    @PostMapping("/pdf-A1")
-    public ResponseEntity<byte[]> genererPdf()
-    {
-        byte[] pdf = gabaritOptionT2.genererReleveT2();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_PDF);
-        headers.setContentDispositionFormData("inline", "releve-notes.pdf");
-
-        return ResponseEntity.ok().headers(headers).body(pdf);
-    }
+//    @PostMapping("/pdf-A1")
+//    public ResponseEntity<byte[]> genererPdf()
+//    {
+//        byte[] pdf = gabaritOptionT2.genererReleveT2();
+//
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.APPLICATION_PDF);
+//        headers.setContentDispositionFormData("inline", "releve-notes.pdf");
+//
+//        return ResponseEntity.ok().headers(headers).body(pdf);
+//    }
 
     @GetMapping(value = "/releve-stat", produces = MediaType.APPLICATION_PDF_VALUE)
     public ResponseEntity<byte[]> genererPdfStatistiques() {
